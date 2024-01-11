9c2fe54b0d438adaa7681004261583d307518919
Peter Shen <peter.shen@nokia-sbell.com>
2024-01-11 06:44:38 +0000

    setup bootstrap

---
+++ cmake/platforms/aarch64.cmake
@@ -0,0 +1,14 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR aarch64)
+
+set(CMAKE_CROSS_PREFIX  "aarch64-buildroot-linux-gnu")
+set(CMAKE_C_COMPILER    "aarch64-buildroot-linux-gnu-gcc")
+set(CMAKE_CXX_COMPILER  "aarch64-buildroot-linux-gnu-g++")
+set(CMAKE_AR            "aarch64-buildroot-linux-gnu-ar")
+set(CMAKE_LINKER        "aarch64-buildroot-linux-gnu-g++")
+set(CMAKE_LD            "aarch64-buildroot-linux-gnu-ld")
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
